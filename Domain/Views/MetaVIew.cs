1beec8200b8d9c18b2d1ebe7e3cb2a47641b8740
Miguel Angel Hobak koh <Maikol_9999@hotmail.com.ar>
2023-03-11 13:00:55 -0600

    Se configura la vista en el contexto

--- Domain/Views/MetaVIew.cs
+++ Domain/Views/MetaVIew.cs
@@ -1,13 +1,6 @@
-﻿using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Views
+﻿namespace Domain.Views
 {
-    internal class MetaVIew
+    public class MetaView
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
--- Persistence/TodoDbContext.cs
+++ Persistence/TodoDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities;
+using Domain.Views;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Mappings;
 
@@ -13,6 +14,9 @@ namespace Persistence
         public DbSet<Meta> Metas { get; set; }
         public DbSet<Tarea> Tareas { get; set; }
 
+        //Vista
+        public DbSet<MetaView> MetaView { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             //Aplicar configuracion de la tabla de metas
@@ -20,6 +24,12 @@ namespace Persistence
 
             //Aplicar configuracion de la tabla de tareas
             builder.ApplyConfiguration(new TareaMap());
+
+            ///Registrar la vista
+            builder
+            .Entity<MetaView>()
+            .ToView(nameof(MetaView))
+            .HasKey(t => t.Id);
         }
     }
 }
