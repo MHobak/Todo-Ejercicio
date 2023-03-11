cc2a53a87266c7da0f673288898d835f96ae4e55
Miguel Angel Hobak koh <Maikol_9999@hotmail.com.ar>
2023-03-11 12:49:30 -0600

    Migrción para funciones

---
+++ Persistence/Migrations/20230311182626_CreateFunctions.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateFunctions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE FUNCTION dbo.GetTotalTareas(@MetaId INT)
+                RETURNS INT
+                AS
+                BEGIN
+                    DECLARE @Total iNT;
+                    SET @Total = (SELECT COUNT(1) FROM Tarea WHERE MetaId = @MetaId)
+                    RETURN @Total
+                END
+            ");
+
+            migrationBuilder.Sql(@"
+                CREATE FUNCTION dbo.GetTotalTareasCompletadas(@MetaId INT)
+                RETURNS INT
+                AS
+                BEGIN
+                    DECLARE @Total iNT;
+                    SET @Total = (SELECT COUNT(1) FROM Tarea WHERE MetaId = @MetaId AND Estado = 'Completada')
+                    RETURN @Total
+                END
+            ");
+
+            migrationBuilder.Sql(@"
+                CREATE FUNCTION dbo.GetTareasCompletionPercentage(@MetaId INT)
+                RETURNS DECIMAL(5,2)
+                AS
+                BEGIN
+                    DECLARE @Total DECIMAL(5,2);
+                    SET @Total = (
+                        SELECT 
+			                (
+				                CASE
+				                -- Si las tareas de la meta = 0 regresar 0 para no dividir entre 0
+				                WHEN (SELECT COUNT(1) FROM Tarea WHERE Tarea.MetaId = @MetaId) = 0.0 THEN 0.0 
+				                ELSE (
+				                --dividir la cantidad de tareas completadas entre el total
+					                (SELECT COUNT(1) FROM Tarea WHERE Tarea.MetaId = @MetaId AND Tarea.Estado = 'Completada') * 100.0 / 
+					                (SELECT COUNT(1) FROM Tarea WHERE Tarea.MetaId = @MetaId)
+				                ) END
+			                )
+		                )
+                    RETURN @Total
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION dbo.GetTotalTareas");
+            migrationBuilder.Sql("DROP FUNCTION dbo.GetTotalTareasCompletadas");
+            migrationBuilder.Sql("DROP FUNCTION dbo.GetTareasCompletionPercentage");
+        }
+    }
+}
--- Persistence/Scripts/Functions/Metas/GetTareasCompletionPercentage.sql
+++ Persistence/Scripts/Functions/Metas/GetTareasCompletionPercentage.sql
@@ -1,18 +1,21 @@
 ﻿CREATE FUNCTION dbo.GetTareasCompletionPercentage(@MetaId INT)
-RETURNS INT
+RETURNS DECIMAL(5,2)
 AS
 BEGIN
     DECLARE @Total DECIMAL(5,2);
     SET @Total = (
-        SELECT CAST(
-			(CASE 
-			WHEN (SELECT COUNT(1) FROM Tarea WHERE Tarea.MetaId = @MetaId) = 0 THEN 0 -- Si las tareas de la meta = 0 regresar 0 para no dividir entre 0
-			ELSE (
-				(SELECT COUNT(1) FROM Tarea WHERE Tarea.MetaId = @MetaId AND Tarea.Estado = 'Completada') * 100.0 / --dividir la cantidad de tareas completadas entre el total
-				(SELECT COUNT(1) FROM Tarea WHERE Tarea.MetaId = @MetaId)
-			) END) AS DECIMAL(5,2) 
+        SELECT 
+			(
+				CASE
+				-- Si las tareas de la meta = 0 regresar 0 para no dividir entre 0
+				WHEN (SELECT COUNT(1) FROM Tarea WHERE Tarea.MetaId = @MetaId) = 0.0 THEN 0.0 
+				ELSE (
+				--dividir la cantidad de tareas completadas entre el total
+					(SELECT COUNT(1) FROM Tarea WHERE Tarea.MetaId = @MetaId AND Tarea.Estado = 'Completada') * 100.0 / 
+					(SELECT COUNT(1) FROM Tarea WHERE Tarea.MetaId = @MetaId)
+				) END
+			)
 		)
-    )
     RETURN @Total
 END
 
