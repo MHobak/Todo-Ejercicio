4cc30d8f037f16816124635ecbff6d65f32a53ab
Miguel Angel Hobak koh <Maikol_9999@hotmail.com.ar>
2023-03-11 12:55:10 -0600

    Migración para creación de vista

---
+++ Persistence/Migrations/20230311185108_AddMetaView.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMetaView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE VIEW [dbo].[MetaView] AS
+                SELECT Id, Nombre, FechaCreacion, 
+                (dbo.GetTotalTareas(Id)) AS TotalTareas,
+                (dbo.GetTotalTareasCompletadas(Id)) AS TareasCompletadas,
+                (dbo.GetTareasCompletionPercentage(Id)) AS PorcentajeCumplimiento
+                FROM [dbo].[Meta]
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW dbo.MetaView");
+        }
+    }
+}
