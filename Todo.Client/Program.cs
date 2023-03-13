81f80735724dadf22b4df31654d3d459f132e117
Miguel Angel Hobak koh <Maikol_9999@hotmail.com.ar>
2023-03-13 00:29:59 -0600

    Implemantación de Mudblazor ui

--- Todo.Client/Program.cs
+++ Todo.Client/Program.cs
@@ -3,10 +3,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Todo.Client;
 using Todo.Client.Services.Implementations;
 using Todo.Client.Services.Interfaces;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//ui kit
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
